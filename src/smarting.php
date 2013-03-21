1128ba1a0c17400fb788b690b2658cc9812dc49d
mrmuh <info@mrmuh.com>
2013-03-21 13:48:50 +0100

    - taking aspect ratio into account during the breakpoint selection - image resizing implemented (todo: implement resized -> crop workflow)

--- src/smarting.php
+++ src/smarting.php
@@ -12,18 +12,21 @@ class SmartImg
 	/*
 	 * Settings
 	 */
-	var $resolutions	= array(	1024,
-									768,
-									640,
-									320,
-									200,
-									100);
-	var $aspects 		= array(	'16:10',
-									'16:9',
-									'4:3',
-									'1:1');
-
-	var $cacheRootDir	= "/demo/img/cache";
+	var $resolutions		= array(	1024,
+										768,
+										640,
+										320,
+										200,
+										100);
+	var $aspects 			= array(	'16:10',
+										'16:9',
+										'4:3',
+										'1:1');
+	const DEFAULT_ASPECT 	= "orig";
+	const DEFAULT_RESOLUTION= "orig";
+	const JPEG_QUALITY		= 75;
+	
+	var $cacheRootDir		= "/demo/img/cache";
 	
 	/**
 	 * Constructor
@@ -36,14 +39,49 @@ class SmartImg
 	/**
 	 * finds the next larger resolution breakpoint for the given width
 	 * @param integer $width
-	 * @return integer|string:	either the resolution breakpoint or the string 'orig' if
+	 * @return integer|string:	either the resolution breakpoint or defaultWidth if
 	 * 							the requested width is larger than the largest breakpoint
 	 */
-	private function getWidthBreakpoint($width)
+	private function getWidthBreakpoint($width, $dstAspect, $srcDimensions)
 	{
 		// if no width was passed, we don't resize
-		if($width === null)
-			return 'orig';
+		// TODO: depending on the aspect this could not work as aspected
+		//if($width === null)
+		//	return SmartImg::DEFAULT_RESOLUTION;
+		
+		/*
+		 * take the aspect ratio into account to determine the possible width
+		 */
+		// get the src image dimensions
+		$srcWidth = $srcDimensions[0];
+		$srcHeight = $srcDimensions[1];			
+			
+		// determine ratios of src and desired aspect
+		$srcRatio = $srcHeight/$srcWidth;
+		$dstRatio = $dstAspect[1]/$dstAspect[0];
+			
+		// check if the dstRatio fits into the srcRatio
+		if($dstRatio > $srcRatio){
+			/*
+			 * at this point we know that the height of the destination aspect is
+			 * proportional higher than the source height by scaling factor s.
+			 * E.g given a box A with an 2:1 aspect (r_0 = 0.5) and the desired 
+			 * box B with an 1:1 aspect (r_1 = 1);
+			 * The s is determined by the ratio s = r_0 / r_1; the width of B could 
+			 * be determined by A.width * s;
+			 */			
+			$scalingFactor = $srcRatio / $dstRatio;
+			$dstWidth = (int)($srcWidth * $scalingFactor);
+			
+			// if no width was passed, use original width
+			if($width === null)
+				$width = $srcWidth;
+			else
+				// for the breakpoint selection we use the minimum of both widths
+				$width = min($dstWidth,$width);			
+		}
+		
+	
 		
 		// copy and sort the resolutions array, just in case
 		$resolutions = $this->resolutions;
@@ -51,7 +89,7 @@ class SmartImg
 		
 		// if the image is larger than the largest breakpoint, we don't resize
 		if($width >= $resolutions[0])
-			return 'orig';
+			return SmartImg::DEFAULT_RESOLUTION;
 		
 		$resultingBreakpoint = $resolutions[0];
 		foreach($resolutions as $currentBreakpoint)
@@ -79,14 +117,17 @@ class SmartImg
 	 * @param string $aspectDir sanitized folder name (write back!)
 	 * @return the aspect enum from resolutions
 	 */
-	protected function getAspect($aspect, &$aspectVal, &$aspectDir){
+	protected function getAspect($aspect, $srcDimensions, &$aspectVal, &$aspectDir){
 		
 		// determine aspect from list
-		if($aspect!=null && in_array($aspect,$this->aspects))
+		if($aspect!=null && in_array($aspect,$this->aspects)){
 			// convert aspect string to values
 			$aspectVal = $this->getAspectValues($aspect);
-		else
-			$aspect = $this->sourceAspect;
+		}else{
+			// TODO find "nearest aspect"
+			$aspectVal = array($srcDimensions[0], $srcDimensions[1]);
+			$aspect = SmartImg::DEFAULT_ASPECT;
+		}
 			
 		// get dirname of aspect
 		$aspectDir = $this->getSanitizedAspect($aspect);
@@ -114,8 +155,8 @@ class SmartImg
 		$lastSlash = strrpos($srcPath, '/');
 		if($lastSlash !== false)
 		{
-			$srcDir = '/' . substr($srcPath, 0, $lastSlash);
-			$srcFile = substr($srcPath, $lastSlash + 1);
+			$srcDir = '/' . dirname($srcPath);
+			$srcFile = basename($srcPath);
 		}
 	}
 	
@@ -133,6 +174,8 @@ class SmartImg
 		return false;
 	}
 	
+	
+	
 	/**
 	 * @TODO: describe me
 	 * @param string	$srcPath	the absolute path to the image (may not be null)
@@ -149,15 +192,19 @@ class SmartImg
 		// split src into srcDir and srcFile (write-back params!)
 		$this->splitPath($srcPath, $srcDir, $filename);
 		
-		// determine resolution breakpoint
-		$widthBreakpoint = $this->getWidthBreakpoint($width);
-			
+		// determine src image dimensions
+		$srcDimensions = GetImageSize($_SERVER['DOCUMENT_ROOT'].$srcPath);
+		
 		// determine aspect parameters
 		// note: aspectVal and aspectDir are used for writing back from the method
-		$aspect = $this->getAspect($aspect, $aspectVal, $aspectDir);
+		$aspect = $this->getAspect($aspect, $srcDimensions, $aspectVal, $aspectDir);
+			
 		
+		// determine resolution breakpoint
+		$widthBreakpoint = $this->getWidthBreakpoint($width, $aspectVal, $srcDimensions);
+					
 		/*
-		 * calculuate the cache path
+		 * calculate the cache path
 		 * the cache folder is organized as follows:
 		 * [cachefolder][aspect][resolutionbreakpoint]
 		 */
@@ -168,36 +215,83 @@ class SmartImg
 		if(!$this->checkCache($cachePath, $srcPath))
 		{
 			if(!file_exists($_SERVER['DOCUMENT_ROOT'].$cacheDir))
-				mkdir($_SERVER['DOCUMENT_ROOT'].$cacheDir, 0755, true);
+				@mkdir($_SERVER['DOCUMENT_ROOT'].$cacheDir, 0755, true);
 			
+			// check if the breakpoint corresponds to the default flag
+			$widthBreakpointValue = $widthBreakpoint;
+			if($widthBreakpoint === SmartImg::DEFAULT_RESOLUTION)
+				$widthBreakpointValue = $srcDimensions[0];
+			
+
 			/*
-			 * TODO: resize image instead of just copying
+			 * Following resizing code stems from Adaptive Images by Matt Wilcox, licensed under a Creative Commons Attribution 3.0 Unported License.
 			 */
-			copy($_SERVER['DOCUMENT_ROOT'].$srcPath, $_SERVER['DOCUMENT_ROOT'].$cachePath);
+			
+			// the image exentsion
+			$extension = strtolower(pathinfo($_SERVER['DOCUMENT_ROOT'].$srcPath, PATHINFO_EXTENSION));
+			
+			// get width and height
+			$srcWidth = $srcDimensions[0];
+  			$srcHeight = $srcDimensions[1];
+			
+			// first resize the image to the breakpoint maintaining the aspect
+  			$srcRatio = $srcHeight/$srcWidth;
+  			$dstRatio = $aspectVal[1]/$aspectVal[0];
+  			
+			$dstWidth = $widthBreakpointValue;
+			$dstHeight = ceil($dstWidth * $dstRatio);
+			$dstImg = ImageCreateTrueColor($dstWidth, $dstHeight); // re-sized image
+			
+			switch ($extension) {
+				case 'png':
+					$srcImg = @ImageCreateFromPng($_SERVER['DOCUMENT_ROOT'].$srcPath); // original image
+					break;
+				case 'gif':
+					$srcImg = @ImageCreateFromGif($_SERVER['DOCUMENT_ROOT'].$srcPath); // original image
+					break;
+				default:
+					$srcImg = @ImageCreateFromJpeg($_SERVER['DOCUMENT_ROOT'].$srcPath); // original image
+					ImageInterlace($dstImg, true); // Enable interlacing (progressive JPG, smaller size file)
+					break;
+			}
+			
+			if($extension=='png'){
+				imagealphablending($dstImg, false);
+				imagesavealpha($dstImg,true);
+				$transparent = imagecolorallocatealpha($dstImg, 255, 255, 255, 127);
+				imagefilledrectangle($dstImg, 0, 0, $dstWidth, $dstHeight, $transparent);
+			}
+			
+			// TODO: describe me :D 
+			// determine start coordinates in src image
+			if($dstRatio>$srcRatio){
+				$cropBoxWidth = (int)($srcWidth * ($srcRatio / $dstRatio));
+				$cropBoxHeight = (int)($cropBoxWidth * $dstRatio);
+			}else{
+				$cropBoxWidth = (int)($srcWidth);
+				$cropBoxHeight = (int)($srcWidth * $dstRatio);
+			}
+			
+			// resizing the image to the breakpoint width (maintaining the src ratio)
+			ImageCopyResampled($dstImg, $srcImg, 0, 0, (int)(($srcWidth - $cropBoxWidth) / 2),  (int)(($srcHeight - $cropBoxHeight) / 2), $dstWidth, $dstHeight, $cropBoxWidth, $cropBoxHeight); // do the resize in memory
+			ImageDestroy($srcImg);
+			
+			// TODO first resize then crop; note: this is only possible in some cases ( srcAspect <= dstAspect)
 			
 			
-// 			// init imagine
-// 			$imagine = new \Imagine\Gd\Imagine();
-// 			// open original image
-// 			$image = $imagine->open($absPath);
-// 			// get size
-// 			$size = $image->getSize();
-// 			// resize by maintaining the aspect
-// 			$newSize = $size->widen($widthBreakpoint);
-// 			$image->resize($newSize);
-// 			/*
-// 			 *	Aspect is set -> crop image
-// 			*/
-// 			if($aspect != $this->sourceAspect){
-// 			// define box of cropped image
-// 				$cropBox = new Imagine\Image\Box(	$newSize->getWidth(),
-// 													$newSize->getWidth() * ($aspectVal[1]/$aspectVal[0]));
-// 				$image = $image->thumbnail($cropBox);
-// 				var_dump($size->getWidth() * ($aspectVal[1]/$aspectVal[0]));
-// 			}
-// 			// store processed image
-// 			$image->save($imagePath);
-			
+			// save the new file in the appropriate path, and send a version to the browser
+			switch ($extension) {
+				case 'png':
+					$gotSaved = ImagePng($dstImg, $_SERVER['DOCUMENT_ROOT'].$cachePath);
+					break;
+				case 'gif':
+					$gotSaved = ImageGif($dstImg, $_SERVER['DOCUMENT_ROOT'].$cachePath);
+					break;
+				default:
+					$gotSaved = ImageJpeg($dstImg, $_SERVER['DOCUMENT_ROOT'].$cachePath, SmartImg::JPEG_QUALITY);
+					break;
+			}
+			ImageDestroy($dstImg);				
 			
 			// set mtime
 			touch($_SERVER['DOCUMENT_ROOT'].$cachePath, filemtime($_SERVER['DOCUMENT_ROOT'].$srcPath));
